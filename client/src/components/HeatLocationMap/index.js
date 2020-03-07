307fd9891aa3f57a458205f363936674d2e89389
Liz <52430633+emariebetson@users.noreply.github.com>
2020-03-07 00:45:13 -0600

    adding heatsearchmaps to components--hotbars page

---
+++ client/src/components/HeatLocationMap/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+function HeatLocationMap() {
+  return (
+        <iframe src="http://data.cityofchicago.org/dataset/Chicago-Bar-Heat-Map/n73w-tj74/embed?width=800&height=600" width={800} height={600} style={{border: 0, padding: 0, margin: 0}} />
+      );
+    }
+
+  export default HeatLocationMap;
---
+++ client/src/components/SearchMap/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+function SearchMap() {
+  return (
+        <iframe src="http://data.cityofchicago.org/dataset/Chicago-Lines/ipht-8dja/embed?width=800&height=600" width={800} height={600} style={{border: 0, padding: 0, margin: 0}} />
+      );
+    }
+
+  export default SearchMap;
